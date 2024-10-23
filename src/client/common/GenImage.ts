a91aafd50524118375c2eec74d2c40f7db496e30
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2024-10-23 17:54:45 +0200

    update GenImage.ts

--- src/client/common/GenImage.ts
+++ src/client/common/GenImage.ts
@@ -1,5 +1,37 @@
 import * as Common from "@common";
 
-export type GenImage = {
-    styleTags: string[];
+export type GenImageConstructorPayload = {
+    src: string;
+    tags: string[];
+    products: Common.Product[];
 };
+
+export type GenImage = {
+    src(): string;
+    tags(): string[];
+    products(): Common.Product[];
+};
+export function GenImage(_payload: GenImageConstructorPayload): GenImage {
+    let _src: string;
+    let _tags: string[];
+    let _products: Common.Product[];
+    /***/ {
+        _src = _payload.src;
+        _tags = [];
+        _products = [];
+        return {
+            src,
+            tags,
+            products
+        };
+    }
+    function src(): string {
+        return _src;
+    }
+    function tags(): string[] {
+        return _tags;
+    }
+    function products(): Common.Product[] {
+        return _products;
+    }
+}
